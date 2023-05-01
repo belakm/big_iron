cd0e3041bc25f309bcaf3badb90df85d7e54596a
Mitja Belak <mico@0x0.icu>
2023-05-01 17:05:37 +0200

    Archiving -> moved to meshetar

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,5 +1,6 @@
 // Main modules
 //
+mod big_iron;
 mod book;
 mod database;
 mod formatting;
@@ -22,6 +23,43 @@ use tokio::runtime::Runtime;
 // Logging
 use env_logger::{Builder, Env};
 use log::LevelFilter;
+
+fn main() {
+    // Create
+
+    // Set log level for libs that are too noisy
+    //
+    let mut builder = Builder::from_env(Env::default().default_filter_or("info"));
+    builder.filter_module("sqlx", LevelFilter::Off).init();
+
+    // Init R libs and start the program
+    //
+    match rlang_runner::r_script("renv_install.R", None) {
+        Ok(_) => {
+            let rt = Runtime::new().unwrap();
+            rt.block_on(async {
+                // Call your async functions in sequence
+                let init1 = database::initialize().await;
+                /*let init2 = book::fetch_history("BTCUSDT").await;
+
+                // TODO: find better matching mechanism
+                match (init1, init2) {
+                    (Ok(_), Ok(_)) => {}
+                    (Ok(_), Err(e)) => println!("{:?}", e),
+                    (Err(e), Ok(_)) => println!("{:?}", e),
+                    (Err(e1), Err(e2)) => println!("{:1?} {:2?}", e1, e2),
+                }*/
+            });
+            rt.spawn(book::main());
+            start_rocket().unwrap();
+        }
+        Err(e) => println!("Error in R: {:?}", e),
+    }
+}
+
+// Rocket
+//
+// needs to in the root crate
 
 #[macro_use]
 extern crate rocket;
@@ -55,44 +93,3 @@ async fn start_rocket() -> Result<(), String> {
         Err(e) => Err(e),
     }
 }
-
-fn main() {
-    // Set log level for libs that are too noisy
-    //
-    let mut builder = Builder::from_env(Env::default().default_filter_or("info"));
-    builder.filter_module("sqlx", LevelFilter::Off).init();
-
-    // Init R libs and start the program
-    //
-    match rlang_runner::r_script("renv_install.R", None) {
-        Ok(_) => {
-            let rt = Runtime::new().unwrap();
-            rt.block_on(async {
-                // Call your async functions in sequence
-                let init1 = database::initialize().await;
-                /*let init2 = book::fetch_history("BTCUSDT").await;
-
-                // TODO: find better matching mechanism
-                match (init1, init2) {
-                    (Ok(_), Ok(_)) => {}
-                    (Ok(_), Err(e)) => println!("{:?}", e),
-                    (Err(e), Ok(_)) => println!("{:?}", e),
-                    (Err(e1), Err(e2)) => println!("{:1?} {:2?}", e1, e2),
-                }*/
-            });
-            rt.spawn(book::main());
-            start_rocket().unwrap();
-        }
-        Err(e) => println!("Error in R: {:?}", e),
-    }
-}
-
-// Getting START SYMBOL signal:
-//
-// Signal: (symbol, period)
-//
-// When signal is recieved:
-//
-// 1. Fetch historic data
-// 2. Subscribe to klines
-// 3. Create & Run the analytical model continuessly
